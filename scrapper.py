3697ece5bb29d3068b7dda620cb297fe1a77e3cf
Al-Zaytoun <zanoonhassan6@gmail.com>
2026-03-03 00:19:36 -0700

    Done more work, created a data var to contain the data aswell as modified notification function for future use

--- scrapper.py
+++ scrapper.py
@@ -2,11 +2,12 @@ import requests
 import filecmp
 from bs4 import BeautifulSoup
 from win11toast import toast
+import json
 
 
 TARGET_URL = "https://github.com/SimplifyJobs/Summer2026-Internships?tab=readme-ov-file"
 SWE_TABLE_ID = "user-content--software-engineering-internship-roles"
-
+DATA = []
 def retrieve_data(target_url):
     
     try:
@@ -35,21 +36,39 @@ def parse_data(unparsed_data):
         target_table = parent_header.find_next_sibling("markdown-accessiblity-table")
 
         rows = target_table.find_all("tr")
+
         with open("parsed_output.txt", "w", encoding="UTF-8") as file:
             for row in rows:
+                row_text = row.get_text(separator=" | ", strip=True)
+                parts = row_text.split(" | ")
+
+                if (len(parts) >= 3 and parts[0] != "Company"):
+                    DATA.append({
+                        "Company": parts[0],
+                        "Role": parts[1],
+                        "Location": parts[2]
+                    })
                 file.write(row.get_text(separator=" | ", strip=True) + "\n")
     
     
     except Exception as e:
         print(f"An unexpected error occured: {e}")
-        return 
+        return []
+    
+    return DATA
 
-def handle_notification(company, job, location):
-    toast(f"New Job Update: {company}: {job}\n{location}")
+def handle_notification(length, company, job, location):
+    if length >= 1:
+        toast(f"New Job Update: {company}: {job}\n{location}")
+    else:
+        toast(f"New Job Updates:\n{length} added jobs.")
 
 def is_files_different(file1, file2):
     return filecmp.cmp(file1, file2)
 
 def main():
-    
-    parse_data(UNPARSED_DATA)
+    pass
+
+data = parse_data(UNPARSED_DATA)
+
+
